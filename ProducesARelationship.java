0992a8050efa7e45187765fa2a8df62346d00e70
ZainabAafia <104630186+ZainabAafia@users.noreply.github.com>
2022-04-30 10:13:44 +0530

    Add files via upload

---
+++ ProducesARelationship.java
@@ -0,0 +1,50 @@
+
+public class ProducesARelationship {
+
+	public static void main(String[] args) {
+		
+	Tune t=new Tune();
+	t.pressKeys();
+	
+		}
+			
+               
+	}
+class Music{
+	
+}
+	class Instrument{
+		
+	}
+	class Keys{
+		
+	}
+	class BlackKey{
+		
+	}
+	class WhiteKey{
+	}
+	
+	
+	class Piano extends Instrument{    //is A
+	  BlackKey b=new BlackKey();       //hasA
+	  WhiteKey w=new WhiteKey();       //hasA
+		
+	
+	
+	
+	
+}
+class Tune             //producesA
+
+	{
+	void pressKeys() {
+		System.out.println("New Tune is being played");
+	
+	
+	}
+	
+		
+		}
+
+
